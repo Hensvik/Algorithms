0c8338b30390643f7d726d764cd91aa4f3405ba2
Hensvik <872304091@qq.com>
2021-11-19 17:59:19 +0800

    20211119

--- src/basePromote/class07/Code02_CoinsMin.java
+++ src/basePromote/class07/Code02_CoinsMin.java
@@ -1,7 +1,20 @@
 package basePromote.class07;
 
+//换钱的最少货币数
+//【题目】
+//给定数组 arr，arr 中所有的值都为正数且不重复。每个值代表一种面值的货币，每种面值
+//的货币可以使用任意张，再给定一个整数 aim，代表要找的钱数，求组成 aim 的最少货币
+//数。
+//【举例】
+//arr=[5,2,3]，aim=20。
+//4 张 5 元可以组成 20 元，其他的找钱方案都要使用更多张的货币，所以返回 4。
+//arr=[5,2,3]，aim=0。
+//不用任何货币就可以组成 0 元，返回 0。
+//arr=[3,5]，aim=2。
+//根本无法组成
 public class Code02_CoinsMin {
 
+	//aim表示最终要达成的目标
 	public static int minCoins1(int[] arr, int aim) {
 		if (arr == null || arr.length == 0 || aim < 0) {
 			return -1;
